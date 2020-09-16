fd5f73e143abd09e3c7ce988438bbf5cb5de7228
nightomicron <shadowbat481@gmail.com>
2020-09-16 15:57:49 +0200

    V 0.6
    +Added introduction and project Ur description
    +Added hungarian language (mert a magyar nyelv szep)
    +Moved the JQuery scripts to their own files
    +Organized folders

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+				
+	/*phone position*/
+	$('#phone_icon').css({left: ($(window).width() / 3) - 230});
+	$('#phone_text').css({left: ($(window).width() / 3) - 220});
+				
+	/*email position*/
+	$('#email_icon').css({left: ($(window).width() / 3 * 2) - 170})
+	$('#email_text').css({left: ($(window).width() / 3 * 2) - 230});
+				
+});
